95074dd96befc80d39588ecc31f3e9383e997064
Felpx06 <walvegamert24@gmail.com>
2024-08-05 15:54:48 -0300

    mudei o nome da app

--- projeto_cordel/settings.py
+++ projeto_cordel/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pag_login'
+    'paginas'
 ]
 
 MIDDLEWARE = [
--- projeto_cordel/urls.py
+++ projeto_cordel/urls.py
@@ -2,8 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from paginas.views import projeto_cordel
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login.html/', views.login.html, name='login.html'),
+    path('', views.login),
 ]

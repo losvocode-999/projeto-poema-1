43e382a8f575a43a4c62d76d7ca0524624bbd9a9
Felpx06 <walvegamert24@gmail.com>
2024-08-08 10:54:19 -0300

    ta com um bug no banco de dados

--- todos/models.py
+++ todos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class usuarios(models.Model):
+class usuario(models.Model):
     id_usuarios = models.AutoField(primary_key=True)
     Nome = models.TextField(max_length=255)
     senha = models.TextField()
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from.models import usuario
 
 def login(request):
     return render(request,"todos/login.html")
 
 def usuarios(request):
-    pass
+    novo_usuario = usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('senha')
+    novo_usuario.save()
+
+    return render(request,'usuario/usuarios.html',usuarios)
+
+usuario = {
+    'usuarios': usuario.objects.all()
+}
